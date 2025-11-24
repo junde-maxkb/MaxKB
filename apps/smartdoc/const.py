b98656ccbe0f857e0bc59f7129c87aa06f2a3621
YunHai <yunhai@yhnotes.com>
2025-11-25 00:28:59 +0800

    feat: add CNKI document search functionality and integrate with UI

--- apps/smartdoc/const.py
+++ apps/smartdoc/const.py
@@ -9,6 +9,6 @@ __all__ = ['BASE_DIR', 'PROJECT_DIR', 'VERSION', 'CONFIG']
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.dirname(BASE_DIR)
 VERSION = '1.0.0'
-# CONFIG = ConfigManager.load_user_config(root_path=os.path.abspath('/opt/maxkb/conf'))
-CONFIG = ConfigManager.load_user_config(root_path=os.path.abspath('/Users/yunhai/Documents/CodeData/Project/MaxKB/conf'))
+CONFIG = ConfigManager.load_user_config(root_path=os.path.abspath('/opt/maxkb/conf'))
+# CONFIG = ConfigManager.load_user_config(root_path=os.path.abspath('/Users/yunhai/Documents/CodeData/Project/MaxKB/conf'))
 
